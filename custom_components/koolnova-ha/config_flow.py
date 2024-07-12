e6647527efcf91516808de2ea0802239f986aea7
Marco Tisi <2930004+marcotisi@users.noreply.github.com>
2024-07-12 15:38:49 +0200

    Add config flow

--- custom_components/koolnova-ha/config_flow.py
+++ custom_components/koolnova-ha/config_flow.py
@@ -2,7 +2,6 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from . import DOMAIN
 
@@ -17,10 +16,16 @@ class KoolnovaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            if user_input["username"] in configured_instances(self.hass):
+            username = user_input["username"]
+            password = user_input["password"]
+            if username in configured_instances(self.hass):
                 errors["base"] = "username_exists"
             else:
-                return self.async_create_entry(title=user_input["username"], data=user_input)
+                try:
+                    await self.hass.async_add_executor_job(self.authenticate, username, password)
+                    return self.async_create_entry(title=username, data=user_input)
+                except Exception as e:
+                    errors["base"] = "auth"
 
         data_schema = vol.Schema({
             vol.Required("username"): str,
@@ -28,3 +33,11 @@ class KoolnovaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+
+    def authenticate(self, username, password):
+        import requests
+        response = requests.post('https://api.koolnova.com/auth/v2/login/', json={
+            'username': username,
+            'password': password
+        })
+        response.raise_for_status()
